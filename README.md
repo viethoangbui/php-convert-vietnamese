e9307a6e134a3a6608644355a5d2a0c7a97f4140
vietbh <vietcone@gmail.com>
2023-03-24 20:24:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Package convert Vietnamese
---
+++ test/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__. '/../vendor/autoload.php';
+
+use Vietbh\ConvertVietnamese\Convert;
+
+echo Convert::convertLatin('Bùi Hoàng Việt');
